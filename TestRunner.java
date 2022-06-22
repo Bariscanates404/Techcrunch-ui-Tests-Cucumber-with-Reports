8aa371a31945d7d60f1de25a17ab65dc6e4b2950
Baris Can Ateş <103248330+Bariscanates404@users.noreply.github.com>
2022-06-22 18:22:37 +0300

    TestRunner.java
    TestRunner.java

--- TestRunner.java
+++ TestRunner.java
@@ -17,20 +17,5 @@ import org.junit.runner.RunWith;
 )
 
 public class TestRunner {
-    /*
-    Bir framework'de bir tek Runner class'i yeterli olabilir
-    Runner class'inda class body'sinde hic bir sey olmaz
-    Runner class'imizi onemli yapan 2 adet annotaion vardir
-    @RunWith(Cucumber.class) notasyonu Runner class'ina calisma ozelligi katar
-    Bu notasyon oldugu icin Cucumber framework'umuzde JUnit kullanmayi tercih ediyoruz
-
-    features : Runner dosyasinin feature dosyalarini nereden bulacagini tarif eder
-    glue : step definitions dosyalarini nerede bulacagimizi gosterir
-    tags : o an hangi tag'i calistirmak istiyorsak onu belli eder
-
-   dryRun : iki secenek var
-   dryRun=true; yazdigimizda Testimizi calistirmadan sadece eksik adimlari bize verir
-   deyRun=false yazdigimizda testlerimizi calistirir
-
-     */
+    
 }
